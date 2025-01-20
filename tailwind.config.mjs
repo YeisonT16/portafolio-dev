32ed5767bd4036881e0e9ec78ff610c3d1f86d8d
Yeison Silva <yeisonsilvat06@gmail.com>
2025-01-19 23:23:32 -0500

    animaciones de prueba para los botones de contacto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,35 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			// animation: {
+			// 	box: 'box 3s infinite',
+			// 	move:'move 3s infinite',
+			// },
+			// 	keyframes: {
+			// 	box: {
+			// 		'0%': {boxShadow: '#27272c' },
+			// 		'50%': {boxShadow: '0 0 25px cyan'},
+			// 		'100%': { boxShadow: '#27272c' },
+			// 	},
+			// 	move: {
+			// 		'0%': {
+			// 			transform: translateX(0),
+			// 		},
+			// 		'50%': {
+			// 			transform: translateX('5px'),
+			// 		},
+			// 		'100%': {
+			// 			transform: translateX(0),
+			// 		}
+			// 	},
+			// },
+		},
+	},
+	variants: {
+		extend: {
+			before: ['hover'],
+		},
 	},
 	plugins: [],
 }
