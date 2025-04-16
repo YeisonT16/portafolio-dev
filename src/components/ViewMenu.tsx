0c1dc614a9b4850f16e14a944ab9be3225cb60ff
Yeison Silva <yeisonsilvat06@gmail.com>
2025-04-15 23:45:40 -0500

    Se elimino el componente NavigationMenu del index

--- src/components/ViewMenu.tsx
+++ src/components/ViewMenu.tsx
@@ -1,21 +1,22 @@
 import { useState } from "react"
-import NavigationMenu from "./NavigationMenu"
+import NavigationMenu from "./NavigationMenu.tsx"
 
 
 export default function ViewMenu({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(false)
   const handleClick = () => {
     setIsOpen(!isOpen)
+    console.log("click")
   }
  const isView = isOpen ? "hidden" : "flex"
 
 return (
   <>
-    <section
+    <button
       className={`hidden max-[555px]:flex items-center justify-center absolute z-10 right-2 top-2 size-9 rounded-full border-transparent hover:-border-violet-600/50 border-2 hover:text-indigo-500 hover:border-indigo-500/50 transform duration-500 cursor-pointer hover:bg-black`} onClick={() => {handleClick}}
     >
       {children}
-    </section>
+    </button>
     <NavigationMenu isView={isView} />
   
   </>
